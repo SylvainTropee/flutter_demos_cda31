7e2d6ba35656e4900c6146e21d325508a6d9e101
stropee <stropee@campus-eni.fr>
2024-09-26 12:07:27 +0200

    mod 8 demo 1

--- lib/mod8/demo1page1.dart
+++ lib/mod8/demo1page1.dart
@@ -9,9 +9,13 @@ class PageOne extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Text("PAGE 1 !", style: TextStyle(fontSize: 18)),
-          ElevatedButton(onPressed: () {
-            Navigator.pushNamed(context, "/page-two", arguments: 456);
-          }, child: Text("GO TO PAGE 2 !"))
+          ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, "/page-two", arguments: 456);
+                //permet de supprimer la flèche retour en remplaçant la page de base
+                // Navigator.pushReplacementNamed(context, "/page-two", arguments: 456);
+              },
+              child: Text("GO TO PAGE 2 !"))
         ],
       ),
     );
